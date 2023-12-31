390ebd9400c03ac155c46caf05e589ba712bce6a
Alperen Aktuğ <aktugalperen9@gmail.com>
2023-12-31 21:21:54 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+
+import  logo from  './../assets/Images/logo.png'
+
+function Header() {
+  return (
+    <div>
+       <img src={logo} width={60} height={60}/>
+       <div>
+        <input type="text" />
+       </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+    Welcome to My Channel!!!
+      
+    </div>
+  )
+}
+
+export default Home
